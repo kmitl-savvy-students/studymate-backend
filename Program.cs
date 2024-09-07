50a18d9831727a733cef0b2cbcfc31326e6bfd99
kornyellow <kornkubzaza@gmail.com>
2024-09-07 16:47:06 +0700

    Change project to Visual Studio project

--- Program.cs
+++ Program.cs
@@ -1,28 +1,19 @@
-using Microsoft.EntityFrameworkCore;
-using StudyMate.Data;
-using StudyMate.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// My Services
-builder.Services.AddScoped<UserService>();
-
+// Add services to the container.
 builder.Services.AddControllers();
 
-// My Database Contexts
-builder.Services.AddDbContext<UserManagementContext>(options =>
-	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
