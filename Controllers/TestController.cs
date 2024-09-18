b4e7d8000273539abb8a748b7c956331d79124e2
thanatat64 <thanatat64@gmail.com>
2024-09-19 06:36:24 +0700

    test update something on pipeline

---
+++ Controllers/TestController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using studymate_backend.Controllers.Core;
+using studymate_backend.Enums;
+using studymate_backend.Helper;
+using studymate_backend.Models.Core;
+using studymate_backend.Models.StudyMate.Raw.Request;
+using studymate_backend.Services;
+
+namespace studymate_backend.Controllers;
+
+[ApiController]
+[Route("api/test")]
+public class UserController(UserTokenService userTokenService) : BaseController
+{
+    [HttpPost]
+    public BaseResponse Get(RequestUser requestUser)
+    {
+        if (!SDMString.IsValid(requestUser.UserTokenId, 64, 64))
+            return new BaseResponse(EnumResponseCode.FIELDS_INVALID);
+
+        var userTokenId = SDMString.cleanAndTrim(requestUser.UserTokenId);
+
+        // Verify token
+        var userToken = userTokenService.Get(userTokenId);
+        if (userToken == null)
+            return new BaseResponse(EnumResponseCode.UNAUTHORIZED);
+
+        return new BaseResponse(EnumResponseCode.OK, userToken.User.Serialized());
+    }
+}
