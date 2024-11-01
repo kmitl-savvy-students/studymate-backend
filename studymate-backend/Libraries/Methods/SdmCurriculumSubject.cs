2509872746dd8b523866083052deb73dabfdd8a7
Horii <nichapat.uah@gmail.com>
2024-11-01 11:02:45 +0700

    add curriculumsubject/get and curriculumsubject/get{subject_id}

---
+++ studymate-backend/Libraries/Methods/SdmCurriculumSubject.cs
@@ -0,0 +1,65 @@
+﻿using System.Data;
+using studymate_backend.Libraries.Database;
+using studymate_backend.Libraries.Models;
+
+namespace studymate_backend.Libraries.Methods;
+
+public class SdmCurriculumSubject : ISdmBaseMethod<CurriculumSubject>
+{
+    public static string tableName => "cu_curri_subject";
+
+    public static SdmPgsqlSelect getSelectObj()
+    {
+        var builderSelect = new SdmPgsqlSelect(tableName);
+        return builderSelect;
+    }
+    public static List<CurriculumSubject> processQuery(SdmPgsqlQuery query, bool isArray = false)
+    {
+        var result = new List<CurriculumSubject>();
+
+        var reader = query.getReader();
+        if (reader == null)
+            return result;
+
+        while (reader.Read())
+        {
+            var curriculumSubject = new CurriculumSubject(
+                reader.GetString(0),
+                reader.GetInt32(1),
+                reader.GetInt32(2),
+                reader.GetInt32(3),
+                reader.GetString(4),
+                reader.GetString(5),
+                reader.GetString(6),
+                reader.GetString(7)
+            );
+
+            result.Add(curriculumSubject);
+
+            if (!isArray)
+                return result;
+        }
+
+        return result;
+    }
+
+    public static List<CurriculumSubject> getAll()
+    {
+        var select = getSelectObj();
+
+        var result = processQuery(new SdmPgsqlQuery(select), true);
+        return result;
+    }
+    
+    public static CurriculumSubject? getById(string subject_id)
+    {
+        var select = getSelectObj();
+        select.whereEqual("subject_id", subject_id.ToString());
+
+        var result = processQuery(new SdmPgsqlQuery(select), true);
+        if (result.Count == 0)
+            return null;
+        return result[0];
+    }
+    
+}
---
+++ studymate-backend/Libraries/Models/CurriculumSubject.cs
@@ -0,0 +1,22 @@
+﻿namespace studymate_backend.Libraries.Models;
+
+public class CurriculumSubject(
+    string subject_id,
+    int c_cat_id,
+    int c_group_id,
+    int c_subgroup_id,
+    string curri_id,
+    string year,
+    string normal_flag,
+    string coop_flag
+) : IBaseModel
+{
+    public string subject_id { get; set; } = subject_id;
+    public int c_cat_id { get; set; } = c_cat_id;
+    public int c_group_id { get; set; } = c_group_id;
+    public int c_subgroup_id { get; set; } = c_subgroup_id;
+    public string curri_id { get; set; } = curri_id;
+    public string year { get; set; } = year;
+    public string normal_flag { get; set; } = normal_flag;
+    public string coop_flag { get; set; } = coop_flag;
+}
