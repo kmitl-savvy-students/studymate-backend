f6439ed792726bc611c02c47163862820ecfad39
Horii <nichapat.uah@gmail.com>
2024-11-01 17:22:10 +0700

    add subject/get and subject/get{subject_id}

---
+++ studymate-backend/Libraries/Methods/SdmSubject.cs
@@ -0,0 +1,75 @@
+﻿using System.Data;
+using studymate_backend.Libraries.Database;
+using studymate_backend.Libraries.Models;
+
+namespace studymate_backend.Libraries.Methods;
+
+public class SdmSubject : ISdmBaseMethod<Subject>
+{
+    public static string tableName => "subject";
+
+    public static SdmPgsqlSelect getSelectObj()
+    {
+        var builderSelect = new SdmPgsqlSelect(tableName);
+        return builderSelect;
+    }
+    public static List<Subject> processQuery(SdmPgsqlQuery query, bool isArray = false)
+    {
+        var result = new List<Subject>();
+
+        var reader = query.getReader();
+        if (reader == null)
+            return result;
+
+        while (reader.Read())
+        {
+            var subject = new Subject(
+                reader.GetString(0),
+                reader.GetString(1),
+                reader.GetString(2),
+                reader.GetInt32(3),
+                reader.GetInt32(4),
+                reader.GetInt32(5),
+                reader.GetString(6),
+                reader.GetString(7),
+                reader.GetInt32(8),
+                reader.GetString(9),
+                reader.GetInt32(10),
+                reader.GetInt32(11),
+                reader.GetString(12),
+                reader.GetString(13),
+                reader.GetString(14),
+                reader.GetString(15),
+                reader.GetString(16),
+                reader.GetDateTime(17)
+            );
+
+            result.Add(subject);
+
+            if (!isArray)
+                return result;
+        }
+
+        return result;
+    }
+
+    public static List<Subject> getAll()
+    {
+        var select = getSelectObj();
+
+        var result = processQuery(new SdmPgsqlQuery(select), true);
+        return result;
+    }
+    
+    public static Subject? getById(string subject_id)
+    {
+        var select = getSelectObj();
+        select.whereEqual("subject_id", subject_id.ToString());
+
+        var result = processQuery(new SdmPgsqlQuery(select), true);
+        if (result.Count == 0)
+            return null;
+        return result[0];
+    }
+    
+}
---
+++ studymate-backend/Libraries/Models/Subject.cs
@@ -0,0 +1,42 @@
+﻿namespace studymate_backend.Libraries.Models;
+
+public class Subject(
+    string subject_id,
+    string subject_tname,
+    string subject_ename,
+    int credit,
+    int lect_hr,
+    int prac_hr,
+    string prerequisite,
+    string detail,
+    int self_hr,
+    string prerequisite2,
+    int lock_ed,
+    int precondition,
+    string status,
+    string subject_type,
+    string prerequisite3,
+    string prerequisite4,
+    string prerequisite5,
+    DateTime last_modified
+) : IBaseModel
+{
+    public string subject_id { get; set; } = subject_id;
+    public string subject_tname { get; set; } = subject_tname;
+    public string subject_ename { get; set; } = subject_ename;
+    public int credit { get; set; } = credit;
+    public int lect_hr { get; set; } = lect_hr;
+    public int prac_hr { get; set; } = prac_hr;
+    public string prerequisite { get; set; } = prerequisite;
+    public string detail { get; set; } = detail;
+    public int self_hr { get; set; } = self_hr;
+    public string prerequisite2 { get; set; } = prerequisite2;
+    public int lock_ed { get; set; } = lock_ed;
+    public int precondition { get; set; } = precondition;
+    public string status { get; set; } = status;
+    public string subject_type { get; set; } = subject_type;
+    public string prerequisite3 { get; set; } = prerequisite3;
+    public string prerequisite4 { get; set; } = prerequisite4;
+    public string prerequisite5 { get; set; } = prerequisite5;
+    public DateTime last_modified { get; set; } = last_modified;
+}
