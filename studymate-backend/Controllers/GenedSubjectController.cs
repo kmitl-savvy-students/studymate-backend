37c527aef12586f9b1f04f1c0683889d0e0ebe3d
Horii <nichapat.uah@gmail.com>
2024-11-02 18:09:59 +0700

    Add api/genedsubject/get

---
+++ studymate-backend/Controllers/GenedSubjectController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using studymate_backend.Libraries.Methods;
+using studymate_backend.Libraries.Models;
+
+namespace studymate_backend.Controllers;
+
+[ApiController]
+[Route("api/genedsubject/get")]
+public class GenedSubjectController : ControllerBase
+{
+    [AllowAnonymous]
+    [HttpGet]
+    public ActionResult<IEnumerable<GenedSubject>> Get()
+    {
+        var genedSubjetcs = SdmGenedSubject.GetAll();
+
+        if (genedSubjetcs.Count == 0)
+            return NotFound(new { message = "Gened subject not found." });
+        return Ok(genedSubjetcs);
+    }
+}
---
+++ studymate-backend/Libraries/Models/GenedSubject.cs
@@ -0,0 +1,10 @@
+﻿namespace studymate_backend.Libraries.Models;
+
+public class GenedSubject(
+    string subject_id,
+    string group_id
+) : IBaseModel
+{
+    public string subject_id { get; set; } = subject_id;
+    public string group_id { get; set; } = group_id;
+}
