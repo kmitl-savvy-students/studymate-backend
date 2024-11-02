375ee85f3e9f718dc17a45285f0303d82c6f73e5
Horii <nichapat.uah@gmail.com>
2024-11-02 17:54:21 +0700

    Add api/genedgroup/get

---
+++ studymate-backend/Libraries/Models/GenedGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace studymate_backend.Libraries.Models;
+
+public class GenedGroup(
+    string id,
+    string group_name
+) : IBaseModel
+{
+    public string id { get; set; } = id;
+    public string group_name { get; set; } = group_name;
+}
