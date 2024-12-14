e3878879cfb2316b5a488a7411c7d1fc0d8705fc
Horii <nichapat.uah@gmail.com>
2024-12-14 23:58:26 +0700

    Update TeachtableSubject

--- studymate-backend/Libraries/Models/TeachtableSubject.cs
+++ studymate-backend/Libraries/Models/TeachtableSubject.cs
@@ -2,7 +2,6 @@
 
 public class TeachtableSubject(
     Teachtable? teachtable,
-    string public_id,
     string subject_id,
     int interested,
     float rating,
@@ -11,7 +10,6 @@ public class TeachtableSubject(
 {
     public int id { get; set; } = id;
     public Teachtable? teachtable { get; set; } = teachtable;
-    public string public_id { get; set; } = public_id;
     public string subject_id { get; set; } = subject_id;
     public int interested { get; set; } = interested;
     public float rating { get; set; } = rating;
