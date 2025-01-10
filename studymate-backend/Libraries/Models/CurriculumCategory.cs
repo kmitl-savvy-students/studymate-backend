c35e13acf8a74542f049a09accae55094d255490
Horii <nichapat.uah@gmail.com>
2025-01-10 21:49:40 +0700

    Add model curriculumCategory

---
+++ studymate-backend/Libraries/Models/CurriculumCategory.cs
@@ -0,0 +1,16 @@
+﻿namespace studymate_backend.Libraries.Models;
+
+public class CurriculumCategory(
+    int c_cat_id,
+    string curri_id,
+    string year,
+    int credit1,
+    int credit2
+) : IBaseModel
+{
+    public int c_cat_id { get; set; } = c_cat_id;
+    public string curri_id { get; set; } = curri_id;
+    public string year { get; set; } = year;
+    public int credit1 { get; set; } = credit1;
+    public int credit2 { get; set; } = credit2;
+}
