a1c9e0c950b687b956ac04f398357ce031a5490c
Horii <nichapat.uah@gmail.com>
2025-01-30 18:18:44 +0700

    Add model of TeachtableSubjectReviewLike

---
+++ studymate-backend/Libraries/Models/TeachtableSubjectReviewLike.cs
@@ -0,0 +1,12 @@
+﻿namespace studymate_backend.Libraries.Models;
+
+public class TeachtableSubjectReviewLike(
+    string user_id,
+    TeachtableSubjectReview teachtable_subject_review_id,
+    int id = 0
+) : IBaseModel
+{
+    public int id { get; set; } = id;
+    public TeachtableSubjectReview teachtable_subject_review_id { get; set; } = teachtable_subject_review_id;
+    public string user_id { get; set; } = user_id;
+}
