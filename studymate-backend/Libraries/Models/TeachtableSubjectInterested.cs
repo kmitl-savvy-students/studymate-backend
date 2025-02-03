363d1caa055a28e3adc20f669c345271b6f80d8c
Horii <nichapat.uah@gmail.com>
2025-02-03 22:55:29 +0700

    Add model of teachtableSubjectInterested

---
+++ studymate-backend/Libraries/Models/TeachtableSubjectInterested.cs
@@ -0,0 +1,13 @@
+﻿namespace studymate_backend.Libraries.Models;
+
+public class TeachtableSubjectInterested(
+    TeachtableSubject? teachtable_subject,
+    string user_id,
+    int id = 0
+) : IBaseModel
+{
+    public int id { get; set; } = id;
+    public TeachtableSubject? teachtable_subject { get; set; } = teachtable_subject;
+    public string user_id { get; set; }
+
+}
