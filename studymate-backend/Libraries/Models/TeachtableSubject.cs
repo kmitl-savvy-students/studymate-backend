d898335cf13a07dab893c54d5525efb1c031ec97
Horii <nichapat.uah@gmail.com>
2025-02-17 20:05:46 +0700

    Add model TeachtableSubject.cs

---
+++ studymate-backend/Libraries/Models/TeachtableSubject.cs
@@ -0,0 +1,18 @@
+﻿namespace studymate_backend.Libraries.Models;
+
+public class TeachtableSubject(
+    Teachtable? teachtable,
+    string subjectId,
+    int interested,
+    float rating,
+    int countOfReview,
+    int id = 0
+) : IBaseModel
+{
+    public int Id { get; set; } = id;
+    public Teachtable? Teachtable { get; set; } = teachtable;
+    public string SubjectId { get; set; } = subjectId;
+    public int Interested { get; set; } = interested;
+    public float Rating { get; set; } = rating;
+    public int CountOfReview { get; set; } = countOfReview;
+}
