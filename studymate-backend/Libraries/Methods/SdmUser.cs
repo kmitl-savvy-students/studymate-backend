f893fbc2e78f3e87e807f1b19ce1d911a6b9c905
Horii <nichapat.uah@gmail.com>
2025-02-17 12:52:53 +0700

    Add function boolean

--- studymate-backend/Libraries/Methods/SdmUser.cs
+++ studymate-backend/Libraries/Methods/SdmUser.cs
@@ -74,4 +74,42 @@ public abstract class SdmUser : ISdmBaseMethod<User>
         var query = SdmMysqlQuery.Execute(update);
         query.CleanUp();
     }
+
+    public static bool Verify(User user, string otpId)
+    {
+        // ดึง OTP จากฐานข้อมูล
+        var otpAuth = SdmOtpAuthentication.GetById(otpId);
+
+        if (otpAuth.UserId != user.Id)
+        {
+            Console.WriteLine("❌ Not Your Otp Token");
+            return false;
+        }
+
+        if (otpAuth == null)
+        {
+            Console.WriteLine("❌ ไม่พบ OTP หรือหมดอายุแล้ว");
+            return false;
+        }
+
+        if (otpAuth.Status != "VERIFIED")
+        {
+            Console.WriteLine("❌ OTP ยังไม่ได้รับการยืนยัน");
+            return false;
+        }
+
+        if (otpAuth.DateExpired.ToDateTime() < DateTime.UtcNow)
+        {
+            Console.WriteLine("❌ OTP หมดอายุแล้ว");
+            return false;
+        }
+
+        // ถ้าทุกอย่างถูกต้อง ให้เพิ่มผู้ใช้
+        Insert(user);
+        return true;
+    }
+
+
+
+
 }
