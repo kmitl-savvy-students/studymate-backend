631fe9cd50bf1af6a28d2cef07bc0dcd2a04c68d
Horii <nichapat.uah@gmail.com>
2025-02-17 21:16:01 +0700

    Add detail

--- studymate-backend/Libraries/Models/TeachtableSubject.cs
+++ studymate-backend/Libraries/Models/TeachtableSubject.cs
@@ -1,12 +1,12 @@
 ﻿namespace studymate_backend.Libraries.Models;
 
 public class TeachtableSubject(
-    Teachtable? teachtable,
-    string subjectId,
-    int interested,
-    float rating,
-    int countOfReview,
-    int id = 0
+    Teachtable? teachtable, //tts_tt_id
+    string subjectId, //tts_sbj_id
+    int interested, //tts_int
+    float rating, //tts_rat
+    int countOfReview, //tts_cor
+    int id = 0 //tts_id
 ) : IBaseModel
 {
     public int Id { get; set; } = id;
