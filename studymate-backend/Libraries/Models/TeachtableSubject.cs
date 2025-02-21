1efd93a60c7b93c5b2ba563d6fa580ed43d3bc3f
Horii <nichapat.uah@gmail.com>
2025-02-21 21:06:39 +0700

    Comment about TeachtableSubject because i want to delete.

--- studymate-backend/Libraries/Models/TeachtableSubject.cs
+++ studymate-backend/Libraries/Models/TeachtableSubject.cs
@@ -1,16 +1,18 @@
-﻿namespace studymate_backend.Libraries.Models;
-
-public class TeachtableSubject(
-    Teachtable? teachtable, //tts_tt_id
-    string subjectId, //tts_sbj_id
-    float rating, //tts_rat
-    int countOfReview, //tts_cor
-    int id = 0 //tts_id
-) : IBaseModel
-{
-    public int Id { get; set; } = id;
-    public Teachtable? Teachtable { get; set; } = teachtable;
-    public string SubjectId { get; set; } = subjectId;
-    public float Rating { get; set; } = rating;
-    public int CountOfReview { get; set; } = countOfReview;
-}
+﻿// namespace studymate_backend.Libraries.Models;
+//
+// public class TeachtableSubject(
+//     Teachtable? teachtable, //tts_tt_id
+//     string subjectId, //tts_sbj_id
+//     int interested, //tts_int
+//     float rating, //tts_rat
+//     int countOfReview, //tts_cor
+//     int id = 0 //tts_id
+// ) : IBaseModel
+// {
+//     public int Id { get; set; } = id;
+//     public Teachtable? Teachtable { get; set; } = teachtable;
+//     public string SubjectId { get; set; } = subjectId;
+//     public int Interested { get; set; } = interested;
+//     public float Rating { get; set; } = rating;
+//     public int CountOfReview { get; set; } = countOfReview;
+// }
