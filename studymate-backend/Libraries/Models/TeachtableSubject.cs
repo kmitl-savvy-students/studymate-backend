8db4446b85094dfe2e67253de47f762439799287
Horii <nichapat.uah@gmail.com>
2025-02-21 19:43:36 +0700

    Delete interested in database

--- studymate-backend/Libraries/Models/TeachtableSubject.cs
+++ studymate-backend/Libraries/Models/TeachtableSubject.cs
@@ -3,7 +3,6 @@
 public class TeachtableSubject(
     Teachtable? teachtable, //tts_tt_id
     string subjectId, //tts_sbj_id
-    int interested, //tts_int
     float rating, //tts_rat
     int countOfReview, //tts_cor
     int id = 0 //tts_id
@@ -12,7 +11,6 @@ public class TeachtableSubject(
     public int Id { get; set; } = id;
     public Teachtable? Teachtable { get; set; } = teachtable;
     public string SubjectId { get; set; } = subjectId;
-    public int Interested { get; set; } = interested;
     public float Rating { get; set; } = rating;
     public int CountOfReview { get; set; } = countOfReview;
 }
