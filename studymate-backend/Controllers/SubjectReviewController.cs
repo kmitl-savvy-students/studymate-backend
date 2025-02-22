5c6c38dcbdcbdd1133f38c7898b7ec35b3b5ac25
Horii <nichapat.uah@gmail.com>
2025-02-22 19:31:28 +0700

    if not found review return []

--- studymate-backend/Controllers/SubjectReviewController.cs
+++ studymate-backend/Controllers/SubjectReviewController.cs
@@ -14,7 +14,7 @@ public class SubjectReviewController : ControllerBase
         var reviews = SdmSubjectReview.GetAll();
         
         if (reviews.Count == 0)
-            return NotFound(new { message = "Reviews not found." });
+            return Ok(new List<SdmSubjectReview>());
         return Ok(reviews);
     }
     
@@ -178,35 +178,6 @@ public class SubjectReviewController : ControllerBase
             return StatusCode(500, new { message = "Error occurred while fetching data.", error = ex.Message });
         }
     }
-    
-    [HttpGet("average/{subjectId}")]
-    public ActionResult<double> GetRating(string subjectId)
-    {
-        try
-        {
-            double review = SdmSubjectReview.GetAverageRatingOfReview(subjectId);
-            return Ok(review);
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(500, new { message = "An error occurred while fetching the review.", error = ex.Message });
-        }
-    }
-    
-    [HttpGet("count/{subjectId}")]
-    public ActionResult<int> GetCount(string subjectId)
-    {
-        try
-        {
-            double review = SdmSubjectReview.GetCountOfReview(subjectId);
-            return Ok(review);
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(500, new { message = "An error occurred while fetching the review.", error = ex.Message });
-        }
-    }
-
     
 }
 
